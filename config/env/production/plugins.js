c6e95bfd3a973ea9dcb78221b17da6629b55e884
varun <varun@lollypop.design>
2021-05-14 12:24:33 +0530

    plugins moved to production

---
+++ config/env/production/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => {
+  console.log(env("CLOUDINARY_FOLDER"));
+
+  return {
+    // ...
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {
+          folder: env("CLOUDINARY_FOLDER"),
+        },
+        delete: {},
+      },
+    },
+    // ...
+  };
+};
